fe3f94a41461c38b3d6ee974c8bc86c7658cdead
Gabriel Pereira Paiz <gabrielpaiz98@gmail.com>
2019-11-01 19:53:43 -0300

    O decoder está pronto, só falta eu tranformar a insttrução LA em Lui ori no array do codigo antes de passar a mesma no decoder

--- IMPLEMENTACAO/Simulador.java
+++ IMPLEMENTACAO/Simulador.java
@@ -6,172 +6,6 @@ import java.util.*;
 //Aqui vai ser nossa main class.
 public class Simulador{
 
-    public static String decToBinario(String dec, int tam){
-        String ret = "";
-        int iDec = Integer.parseInt(dec);
-        if(iDec >= 0){
-            for(int i = 0;i<tam;i++){
-                ret = String.valueOf(iDec%2) + ret;
-                iDec = iDec/2;
-            }
-        }else{
-            iDec += 1;
-            for(int i = 0;i<tam;i++){
-                if(iDec%2 == 0){
-                    ret = "1" + ret;
-                }else{
-                    ret = "0" + ret;
-                }
-                iDec = iDec/2;
-            }
-        }
-        return ret;
-    }
-
-    public static String qRegistrador(String reg){
-        switch(reg){
-            case "$zero": return "00000";
-            case "$at": return "00001";
-            case "$v0": return "00010";
-            case "$v1": return "00011";
-            case "$a0": return "00100";
-            case "$a1": return "00101";
-            case "$a2": return "00110";
-            case "$a3": return "00111";
-            case "$t0": return "01000";
-            case "$t1": return "01001";
-            case "$t2": return "01010";
-            case "$t3": return "01011";
-            case "$t4": return "01100";
-            case "$t5": return "01101";
-            case "$t6": return "01110";
-            case "$t7": return "01111";
-            case "$s0": return "10000";
-            case "$s1": return "10001";
-            case "$s2": return "10010";
-            case "$s3": return "10011";
-            case "$s4": return "10100";
-            case "$s5": return "10101";
-            case "$s6": return "10110";
-            case "$s7": return "10111";
-            case "$t8": return "11000";
-            case "$t9": return "11001";
-            case "$k0": return "11010";
-            case "$k1": return "11011";
-            case "$gp": return "11100";
-            case "$sp": return "11101";
-            case "$fp": return "11110";
-            case "$ra": return "11111";
-            default: System.out.println("Deu na busca dos registradores um erro!!!"+ reg);break;
-        }
-        return "";
-    }
-
-    public static String decoder(String inst, List<String> code, int pc){
-        Boolean control = true;
-        int i = 0;
-        String ret = "";
-        inst = inst.replace(",", " ").replace("  ", " ");
-        String [] vetInst = inst.split(" ");
-        String [] aux;
-
-        while(control){
-            switch(vetInst[i]){
-                case "addu":
-                    ret += "000000";
-                    ret += qRegistrador(vetInst[i+2]);
-                    ret += qRegistrador(vetInst[i+3]);
-                    ret += qRegistrador(vetInst[i+1]);
-                    ret += "00000";
-                    ret += "100001";
-                    control = false;
-                    break;
-                case "addiu":
-                    ret += decToBinario("9", 6);
-                    ret += qRegistrador(vetInst[i+2]);
-                    ret += qRegistrador(vetInst[i+1]);
-                    ret += decToBinario(vetInst[i+3], 16);
-                    control = false;
-                    break;
-                case "lw":
-                    ret += "100011";
-                    aux = vetInst[i+2].replace("(", "/").replace(")", "").split("/");
-                    ret += qRegistrador(aux[1]);
-                    ret += qRegistrador(vetInst[i+1]);
-                    ret += decToBinario(aux[0], 16);
-                    control = false;
-                    break;
-                case "sw":
-                    ret += "101011";
-                    aux = vetInst[i+2].replace("(", "/").replace(")", "").split("/");
-                    ret += qRegistrador(aux[1]);
-                    ret += qRegistrador(vetInst[i+1]);
-                    ret += decToBinario(aux[0], 16);
-                    control = false;
-                    break;
-                case "and":
-                    ret += "000000";
-                    ret += qRegistrador(vetInst[i+2]);
-                    ret += qRegistrador(vetInst[i+3]);
-                    ret += qRegistrador(vetInst[i+1]);
-                    ret += "00000";
-                    ret += decToBinario("36", 6); 
-                    control = false;
-                    break;
-                case "sll":
-                    ret += "000000";
-                    ret += "00000";
-                    ret += qRegistrador(vetInst[i+2]);
-                    ret += qRegistrador(vetInst[i+1]);
-                    ret += decToBinario(vetInst[i+3], 5);
-                    ret += "000000";
-                    control = false;
-                    break;
-                case "srl":
-                    ret += "000000";
-                    ret += "00000";
-                    ret += qRegistrador(vetInst[i+2]);
-                    ret += qRegistrador(vetInst[i+1]);
-                    ret += decToBinario(vetInst[i+3], 5);
-                    ret += "000010";
-                    control = false;
-                    break;
-                case "ori":
-                    ret += decToBinario("13", 6);
-                    ret += qRegistrador(vetInst[i+2]);
-                    ret += qRegistrador(vetInst[i+1]);
-                    ret += decToBinario(vetInst[i+3], 16);
-                    control = false;
-                    break;
-                case "lui":
-                    ret += decToBinario("15", 6);
-                    ret += "00000";
-                    ret += qRegistrador(vetInst[i+1]);
-                    ret += decToBinario(vetInst[i+2], 16);
-                    control = false;
-                    break;
-                case "beq":
-                    ret += decToBinario("4", 6);
-                    ret += qRegistrador(vetInst[i+1]);
-                    ret += qRegistrador(vetInst[i+2]);
-                    for(int j = 0;j<code.size();j++){
-                        if( (code.get(j).substring(0, (vetInst[i+3].length()))).equals(vetInst[i+3])){
-                            pc = j - (pc+1);
-                            break;
-                        }else{
-                            pc = 0;
-                        }   
-                    }
-                    ret += decToBinario(String.valueOf(pc), 16);
-                    control = false;
-                    break;
-                default: System.out.println("Entrou no tratamento de label, se não tem resposta o problema é no Decoder");
-                         i++;
-                         break;
-            }
-        }
-            return ret;
-    }
     public static int Mux(int ent0, int ent1, int op){
         if(op == 0){return ent0;}
         if(op == 1){return ent1;}
@@ -248,6 +82,7 @@ public class Simulador{
     public static void main(String [] args){
         List<String> code = new ArrayList<String>();
         List<String> data = new ArrayList<String>();
+
         String linhas = "";
 
 
@@ -267,15 +102,21 @@ public class Simulador{
               e.getMessage());
         }
 
-
         code = getCodigo(linhas);
         data = getData(linhas);
 
-        System.out.println(code.toString());
-        System.out.println(data.toString());
+        Decoder dec = new Decoder(code);
 
-        System.out.println(decoder(code.get(0), code, 0));
+        String [] memoriaIntrucao = dec.decoder(); // Isso são todas as instruçoes do codigo em binario
+        String [] instHex = dec.decoderHex(); // Isso é todas as instruçoes do codigo em hexa
 
-        //System.out.println(decToBinario("0", 10));
+        for(int i = 0; i<memoriaIntrucao.length ;i++){ 
+            System.out.println(memoriaIntrucao[i]);
+        }
+        System.out.print("\n");
+        for(int i = 0; i<instHex.length ;i++){
+            System.out.println(instHex[i]);
+        }
+        // Não tirei os prints para vocês poderem ver como está
     }
 }
