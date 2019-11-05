8c42c7095a23301d76c3fcb5d4bd4f4fc601e0b4
Julia Alberti <53387912+juAlberti@users.noreply.github.com>
2019-11-05 00:17:15 -0300

    ainda falta algumas coisas

--- IMPLEMENTACAO/ULA.java
+++ IMPLEMENTACAO/ULA.java
@@ -1,27 +1,43 @@
-public class ULA implements Bloco{
-    private int ent1;
-    private int ent2;
-    private int op;
-    private int saida;
-    private int zero;
+public class ULA {
+    public int Zero=0;
 
-    public ULA(int ent1, int ent2, int op){
-        this.ent1 = ent1;
-        this.ent2 = ent2;
-        this.op = op;
-        saida = 0;
-        zero = 0; 
+    public ULA(){
+         
     }
 
-    public int saida(){
-        /* Vamos fazer todas as operações aqui dentro
-        * Ultilizando as entradas e a op que recebemos
-        * pode ser Switch Case
-        * ex: Switch(op){case 0: saida = ent1 + ent2;} ou pode fazer de outro jeito
-        * nós alteraremos a variavel zero aqui tbm, quando nescessario.
-        */
-        return saida;
+    public int srl(int a,int qunt){
+       return a^(qunt/2);
     }
 
-    public int zero(){return zero;}
+    public int sll(int a,int qunt){
+       return a^(qunt*2);
+    }
+
+    public int neg(int a){
+        return ~a;
+    }
+
+    public int or(int a,int b){
+        return a & b;
+    }
+
+    public int and(int a,int b){
+        return a | b;
+    }
+
+    public void op() {
+		
+		switch(operacao) {
+		case 0000: som(); break;
+		case 0001: sub(); break;
+		case 0010: mult(); break;
+        case 0011: div(); break;
+        case 0100: srl(); break;
+        case 0101: sll(); break;
+        case 0110: neg(); break;
+        case 0111: or(); break;
+        case 1000: and(); break;
+		}
+		
+	}
 }
